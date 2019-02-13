0bc1fd525d030375a3dfc2121e1ccdb95901bd02
Matthew <mramirez@nebulaworks.com>
2019-02-12 22:44:19 -0800

    added elb func

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,5 @@
 terraform {
   required_version = "=0.11.11"
-
-  backend "s3" {
-    bucket = "<bucket_name>"
-    key    = "terraform.tfstate"
-    region = "us-west-2"
-  }
 }
 
 provider "aws" {
